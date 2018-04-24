3c455b091fb615ced1feb268fda12aef8ee5e5a9
Benedek Vass <vabe44@gmail.com>
2018-04-24 04:02:30 +0200

    added quotes to library page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,10 +2,6 @@ import { LibraryPage } from './../library/library';
 import { FavoritesPage } from './../favorites/favorites';
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-
 @Component({
   templateUrl: 'tabs.html'
 })
