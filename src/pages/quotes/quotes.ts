6a219f638cc6c95c39502af2d02f2f3136cc6cc8
Benedek Vass <vabe44@gmail.com>
2018-04-25 03:49:30 +0200

    added alert favorite quote

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @Component({
@@ -9,11 +9,34 @@ import { Quote } from '../../data/quote.interface';
 export class QuotesPage implements OnInit {
 
   quoteGroup: {category: string, quotes: Quote[], icon: string};
-  constructor(public navCtrl: NavController, private navParams: NavParams) {
+  constructor(public navCtrl: NavController, private navParams: NavParams, private alertCtrl: AlertController) {
   }
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
   }
 
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind!',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled!');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
