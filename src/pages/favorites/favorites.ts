697b6dcb763ad5f8a79fb44cf6f1e9eacdc0f7bc
Benedek Vass <vabe44@gmail.com>
2018-04-26 02:36:45 +0200

    added sliding item to unfav quote

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -24,13 +24,17 @@ export class FavoritesPage {
     modal.present();
     modal.onDidDismiss((remove: boolean) => {
       if (remove) {
-        this.quotesService.removeQuoteFromFavorites(quote);
-        // this.quotes = this.quotesService.getFavoriteQuotes();
-        const position = this.quotes.findIndex((quoteEl: Quote) => {
-          return quoteEl.id == quote.id;
-        });
-        this.quotes.splice(position, 1);
+        this.onRemoveFromFavorites(quote);
       }
     });
+  }
+
+  onRemoveFromFavorites(quote: Quote) {
+    this.quotesService.removeQuoteFromFavorites(quote);
+    // this.quotes = this.quotesService.getFavoriteQuotes();
+    const position = this.quotes.findIndex((quoteEl: Quote) => {
+      return quoteEl.id == quote.id;
+    });
+    this.quotes.splice(position, 1);
   }
 }
